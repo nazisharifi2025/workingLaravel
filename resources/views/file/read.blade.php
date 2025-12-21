ac5d75087c2de531fe0b322614e42d2cdc254449
nazi sharifi <banonazisharifi@gmail.com>
2025-12-21 22:11:08 +0430

    addednew message

--- resources/views/file/read.blade.php
+++ resources/views/file/read.blade.php
@@ -6,14 +6,18 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hi</h1>
+    <h1 class="text-4xl font-bold">All Songs</h1>
  
         @foreach($sound as $s)
+        <div class="m-5 p-5 border-2 border-green-500 flex flex-col gap-3">
           <h1>{{ $s->senger }}</h1>  
-                  <audio controls>
-        <source src=" asset('storage/' .song->songs) }}" type="audio/mpeg">
-        مرورگر شما از پخش‌کننده صوتی پشتیبانی نمی‌کند.
+          @if($s->songs)
+        <audio controls src="{{ asset('storage/' . $s->songs) }}">
+        <!-- <source  src="{{ asset('storage/' . $s->songs) }}" type="audio/mpeg"> -->
+        Your browsar dos not supported the audio element.
     </audio>
+    @endif
+    </div>
         @endforeach
 </body>
 </html>
